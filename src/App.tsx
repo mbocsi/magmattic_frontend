c3c3c84edd09557ff3418ad84c020a88fc2e4dea
Marton Bocsi <mvbocsi@gmail.com>
2025-02-16 21:30:32 -0600

    Graphs work but really slow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import { useState, useCallback } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import { Line } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LineController,
+	LineElement,
+	PointElement,
+	LinearScale,
+	Title,
+} from "chart.js";
 import "./App.css";
 import { FifoBuffer } from "./lib/buffer";
 
@@ -7,7 +17,34 @@ type VoltageData = { type: "voltage"; val: number };
 type FFTData = { type: "fft"; val: number[][] };
 type DataMessage = VoltageData | FFTData;
 
-const VOLTAGE_LENGTH = 32;
+const VOLTAGE_LENGTH = 500;
+
+ChartJS.register(
+	CategoryScale,
+	LineController,
+	LineElement,
+	PointElement,
+	LinearScale,
+	Title
+);
+
+const options = {
+	// scales: {
+	// 	y: {
+	// 		min: -1,
+	// 		max: 1,
+	// 	},
+	// },
+	animation: {
+		duration: 0, // Set animation duration to 0
+	},
+	// or for specific animations
+	animations: {
+		y: {
+			duration: 0,
+		},
+	},
+};
 
 function App() {
 	const [url, setUrl] = useState<string | null>(null);
@@ -15,7 +52,7 @@ function App() {
 		new FifoBuffer(VOLTAGE_LENGTH, [])
 	);
 	const [fftData, setFftData] = useState<number[][]>([]);
-	const { sendMessage, readyState } = useWebSocket<DataMessage>(url, {
+	const { readyState } = useWebSocket<DataMessage>(url, {
 		onMessage: handleMessage,
 	});
 
@@ -44,15 +81,15 @@ function App() {
 		setUrl(null);
 	}, []);
 
-	const handleChangeUrl = useCallback(() => {
-		console.log("Clicked on change URL");
-		setUrl("10.140.85.161:44444");
-	}, []);
+	// const handleChangeUrl = useCallback(() => {
+	// 	console.log("Clicked on change URL");
+	// 	setUrl("10.140.85.161:44444");
+	// }, []);
 
-	const handleSendControl = useCallback(() => {
-		console.log("Clicked on send control");
-		sendMessage("Hello Magmattic!");
-	}, []);
+	// const handleSendControl = useCallback(() => {
+	// 	console.log("Clicked on send control");
+	// 	sendMessage("Hello Magmattic!");
+	// }, []);
 
 	const connectionStatus = {
 		[ReadyState.CONNECTING]: "Connecting",
@@ -70,19 +107,42 @@ function App() {
 			</p>
 			<button onClick={handleConnect}>Connect</button>
 			<button onClick={handleDisconnect}>Disconnect</button>
-			<button onClick={handleChangeUrl}>Change URL</button>
-			<button onClick={handleSendControl}>Send Messsage</button>
 			<div style={{ display: "flex", flexDirection: "row" }}>
-				<div>
-					{voltageData.getElements().map((v, i) => (
-						<div key={i}>{v}</div>
-					))}
-				</div>
-				<div>
-					{fftData.map((v, i) => (
-						<div key={i}>{v}</div>
-					))}
-				</div>
+				<Line
+					options={options}
+					data={{
+						labels: Array.from(
+							{ length: VOLTAGE_LENGTH },
+							(_, i) => i + 1
+						),
+						datasets: [
+							{
+								data: voltageData.getElements(),
+								backgroundColor: "rgba(255, 99, 132, 1)",
+							},
+						],
+					}}
+					style={{ backgroundColor: "#FFFFFF" }}
+				/>
+				<Line
+					options={options}
+					data={{
+						labels: Array.from(
+							{ length: fftData.length },
+							(_, i) => fftData[i][0]
+						),
+						datasets: [
+							{
+								data: Array.from(
+									{ length: fftData.length },
+									(_, i) => fftData[i][1]
+								),
+								backgroundColor: "rgba(255, 99, 132, 1)",
+							},
+						],
+					}}
+					style={{ backgroundColor: "#FFFFFF" }}
+				/>
 			</div>
 		</div>
 	);
