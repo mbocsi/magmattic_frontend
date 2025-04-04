ea4712c48fda8854a3830fa2b18346c7e7cdbfc6
Marton Bocsi <mvbocsi@gmail.com>
2025-04-04 16:35:04 -0500

    Added react compiler

--- src/components/detectedsignals.tsx
+++ src/components/detectedsignals.tsx
@@ -1,5 +1,5 @@
 import { DataCard, DataList } from "./datacard";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import "./detectedsignals.css";
 export function DetectedSignals({
 	signals,
